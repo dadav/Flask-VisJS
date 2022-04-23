fa64df9154cf49396f2502ebf114b91542231c08
dadav <33197631+dadav@users.noreply.github.com>
2022-04-23 16:18:20 +0200

    update docs

---
+++ examples/home.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template_string
+from flask_visjs import VisJS4, Network
+from pyvis.network import Node
+from pathlib import Path
+
+app = Flask(__name__)
+VisJS4().init_app(app)
+
+
+@app.route('/')
+def index():
+    """
+    Network is based on pyvis's network class, you can check out their docs.
+    """
+    net = Network("1024px", "1024px")
+    home = Path.home()
+    limit = 250
+    current = 0
+    for file in home.glob('**/*'):
+        if current >= limit:
+            break
+        net.add_node(str(file), file.name, shape="box" if file.is_file() else "circle")
+        net.add_node(str(file.parent), file.parent.name, shape="circle")
+        net.add_edge(str(file.parent), str(file))
+        current += 1
+
+    return render_template_string("""
+<html>
+    <head>
+      {{ net.inject_css() }}
+    </head>
+    <body>
+      {{ net.inject_graph() }}
+      {{ net.inject_js() }}
+    </body>
+</html>
+""", net=net)

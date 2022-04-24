28c76288bacf31d3143339ba3e77024a62c5f385
dadav <33197631+dadav@users.noreply.github.com>
2022-04-24 11:40:01 +0200

    fix: improve coverage

--- tests/visjs4/test_visjs.py
+++ tests/visjs4/test_visjs.py
@@ -1,5 +1,6 @@
 import pytest
-from flask import current_app
+from flask import current_app, Flask
+from flask_visjs import VisJS4
 
 
 @pytest.mark.usefixtures('client')
@@ -9,3 +10,13 @@ class TestVisJS4:
 
     def test_blueprint(self):
         assert 'visjs' in current_app.blueprints
+
+    def test_init_via_class(self):
+        app = Flask(__name__)
+        VisJS4(app)
+
+    def test_check_if_extensions_key_is_created(self):
+        app = Flask(__name__)
+        del app.extensions
+        VisJS4().init_app(app)
+        assert hasattr(app, 'extensions') is True
